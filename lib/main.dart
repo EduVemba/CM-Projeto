a9d44a01c2b3135c81a7f5936bcc0f6a5a8d64bb
EduVemba <eduvemba18@gmail.com>
2026-04-27 13:00:11 +0100

    refactor: ficheiros organizados.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:projeto/screens/landing_screen.dart';
+import 'package:projeto/screens/session/landing_screen.dart';
 
 
 void main() {
